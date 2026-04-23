c07d9635a4a045599710c9ab9fb095391393e774
rhzg <rhernandez1999@gmail.com>
2026-04-23 16:04:56 -0400

    move CORS to init.py

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,5 +1,6 @@
+import os
 from flask import Flask
-from app.extensions.extensions import cors
+from flask_cors import CORS
 from app.modules.users.routes import users_bp
 from app.modules.products.routes import products_bp
 from app.modules.carts.routes import carts_bp
@@ -8,7 +9,7 @@ from app.modules.auth.routes import auth_bp
 
 def create_app():
     app = Flask(__name__)
-    cors.init_app(app)
+    CORS(app, resources={r"/api/*": {"origins": os.environ.get("FRONTEND_URL")}})
     app.register_blueprint(users_bp, url_prefix="/api")
     app.register_blueprint(products_bp, url_prefix="/api")
     app.register_blueprint(carts_bp, url_prefix="/api")
